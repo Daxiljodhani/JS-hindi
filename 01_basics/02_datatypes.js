042bc47f34d19abd06777eca60de0ce1e16437ae
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-25 05:23:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+
+let age = 25; //number
+let firstName = "Daxil"; //string
+let isLoggedIn = false; //boolean
+
+//number => 2 to power 53
+//bigint
+//string => '' "" ``
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique identifier
+//object => key:value pair
+
+console.log(typeof "Daxil");
