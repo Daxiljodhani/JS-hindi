d7375b92e231ce7a441bf50c51a05be22fa244ec
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-26 03:27:57 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
 let score = "33"
-console.log(typeof score);
+// console.log(typeof score);
 
 let converttoNum = Number(score)  // convert datatype.
-console.log(converttoNum);
+// console.log(converttoNum);
 
 //"33" => 33
 //"33abc" => NaN
@@ -13,4 +13,30 @@ console.log(converttoNum);
 //0 => false
 //"abc" => true
 //"" => false
+
+//**************************operations**************************
+let value = 100
+let negValue = -value
+console.log(negValue);
+
+console.log(2**3); // 2^3 = 2*2*2 = 8
+
+let str1 = "Hello"
+let str2 = "World"
+let fullStr = str1 + " " + str2
+// console.log(fullStr);
+
+// console.log("2" + 3); // "23"
+// console.log(2 + "3"); // "23"
+// console.log("2" + "3"); // "23"
+// console.log(1 + 3 + "3"); // "43"
+// console.log("2" + 3 + 1); // "231"
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+gamecounter--
+console.log(gamecounter);
+
+
 
