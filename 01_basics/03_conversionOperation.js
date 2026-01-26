1726a41b6457edb797bbfe4a575de20f1069f04a
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-26 03:10:53 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33"
+console.log(typeof score);
+
+let converttoNum = Number(score)  // convert datatype.
+console.log(converttoNum);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+//"" => 0
+//1 => true
+//0 => false
+//"abc" => true
+//"" => false
+
