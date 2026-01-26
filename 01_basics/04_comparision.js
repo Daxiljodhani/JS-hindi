30111ded1150401bea13c8eab560ded7b5018f6f
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-26 03:35:42 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 == 2);
+console.log(2 != 1);
+
+let a = 10
+let b = "10"
+console.log(a == b);  // value check only
+console.log(a === b); // value + datatype check
+console.log(a !== b); // value + datatype check
