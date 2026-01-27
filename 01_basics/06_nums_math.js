156f50a0b4210a75652875209e9dc41bdd516455
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-27 09:25:34 +0000

    nums-math

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,50 @@
+const value = 400
+// console.log(value);
+
+const value1 = new Number(400)
+// console.log(value1);
+// console.log(value1.toString().length);
+// console.log(value1.toFixed(3));
+
+const num = 123.8765
+// console.log(num.toPrecision(3));
+
+const hundred = 1000000000
+// console.log(hundred.toLocaleString());  // US format
+// console.log(hundred.toLocaleString('en-IN'));  //Indian format
+
+// ---------------Maths-----------------
+
+// console.log(Math.abs(-100));    //absolute value.
+// console.log(Math.round(4.678)); //round of value.
+// console.log(Math.ceil(4.2)); // celing function choose upper value.
+// console.log(Math.floor(4.3)); // floor function choose lower value.
+// console.log(Math.min(3,7,8));
+// console.log(Math.max(8,4,9));
+
+// console.log(Math.random()); //take value between 0 to 1.
+// console.log(Math.random()*10 + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);         // it takes value above 10.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
