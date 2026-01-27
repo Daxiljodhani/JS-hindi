5d907ade7680a09ce4c8ff1d2fd08d302d92edff
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-27 08:51:59 +0000

    strings-methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Daxil"
+const repocount = 50
+
+// console.log(name + repocount + "value");
+
+// console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+
+const gamename = new String("Daxil-Jodhani-iii")
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('x'));
+// console.log(gamename.substring(0,5));
+
+const newstring = "  Daxil   "
+console.log(newstring);
+console.log(newstring.trim());             // remove space.
+
+const url = "daxil&j123@gmail.com"
+console.log(url.replace('&','18'));   // replace string part. 
+console.log(url.includes('&j'));    // check given data is in string.
+console.log(gamename.split('-'));   // split given symbol-wise.
+
+
+
+
+
+
+
+
+
