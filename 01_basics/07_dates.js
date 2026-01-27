6df7dc826f93ec3e36e8f94c140495c81012f423
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-27 09:53:44 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,36 @@
+//-------------Dates--------------
+
+let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+
+// console.log(typeof mydate);         // date is an object.
+
+// let newdate = new Date(2016,5,23,5,2)           //months value start from the 0 => jan.
+let newdate = new Date("2022-01-14")               //months start from this format is 01=>jan.
+// console.log(newdate.toLocaleString());
+
+let mycreateddate = Date.now()
+// console.log(mycreateddate);          //milisecond value.
+
+// console.log(Math.round(Date.now()/1000));          //milisecond convert into second.
+
+let date = new Date();
+// console.log(date.getDate());
+// console.log(date.getDay());         //Day value start from 0 => sunday. 
+
+// console.log(date.toLocaleString('default',{weekday:"long"}));  
+
+
+
+
+
+
+
+
+
+
