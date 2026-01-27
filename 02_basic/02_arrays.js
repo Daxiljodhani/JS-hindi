a8a4a510c06efc72a6e6aa4da9cdd0728f0b35c3
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-27 10:47:32 +0000

    array-2

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,33 @@
+const arr1 = [1,2,3]
+const arr2 = [4,5,6,7]
+
+// arr1.push(arr2);
+// console.log(arr1);
+// console.log(arr1[3][2]);
+
+// const allnum = arr1.concat(arr2);
+// console.log(allnum);
+
+// const newnNum = [...arr1,...arr2]        //spread method very easy and mostly useble method.
+// console.log(newnNum);
+
+// const digit = [1,2,[3,4],[5,6,[7,8]]];       //in depth array convert into single array. 
+// const real_digit = digit.flat(Infinity)
+// console.log(real_digit);
+                                                //from method.
+// console.log(Array.from("Daxil"));           //convert any arguments to the array.
+// console.log(Array.from({name: "Daxil"}));    //intersting for interview because this give a empty array.
+
+// let s1 = 100
+// let s2 = 200
+// let s3 = 300                                 //of method.
+// console.log(Array.of(s1,s2,s3));            // give array incluad value 100,200,300.
+
+
+
+
+
+
+
+
+
