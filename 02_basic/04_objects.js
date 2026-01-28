00203b564fa00114e2efa9073e3e886dc43e62bd
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-28 03:59:37 +0000

    API

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -12,12 +12,31 @@ const obj2 =  {3:"c",4:"d"}
 // console.log(obj3);                          //merge to object.
 
 const obj3 = {...obj1,...obj2};             //spread method to merge to obect.
-console.log(obj3);
+// console.log(obj3);
 
-console.log(Object.keys(obj1));             //give only keys.
-console.log(Object.values(obj1));           //give only value.
-console.log(Object.entries(obj1));          //every key value pair convert into array.
-console.log(obj1.hasOwnProperty(2));        //check value is in object or not.(return boolean answer.)
+// console.log(Object.keys(obj1));             //give only keys.
+// console.log(Object.values(obj1));           //give only value.
+// console.log(Object.entries(obj1));          //every key value pair convert into array.
+// console.log(obj1.hasOwnProperty(2));        //check value is in object or not(return boolean answer).
+
+const course = {c_name : "javascript",price : 2000}
+// console.log(course.c_name);              
+const{c_name : name} = course                  //easy for more time access(don't repeat course everytime).
+console.log(name);
+
+/*{
+     "name" : "Daxil"                 //API means without name object. 
+     "age" : "20"
+ } */
+
+/*[
+    {},
+    {}                          //other method for API.
+] */
+
+    
+
+
 
 
 
