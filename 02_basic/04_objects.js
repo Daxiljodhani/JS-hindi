9095ef99080315f511a329a888603eab393fb6ac
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-28 03:39:20 +0000

    objects-2

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,30 @@
+//const app = new Object();         //way to create object.
+// console.log(app);
+
+const app = {name : {username : {firstname : "Daxil",lastname : "jodhnai"}}}
+// console.log(app.name);
+// console.log(app.name.username);
+// console.log(app.name.username.firstname);       //access method of objects.
+
+const obj1 =  {1:"a",2:"b"}
+const obj2 =  {3:"c",4:"d"}
+// const obj3 = Object.assign({},obj1,obj2)       //here {} parameter is optional.
+// console.log(obj3);                          //merge to object.
+
+const obj3 = {...obj1,...obj2};             //spread method to merge to obect.
+console.log(obj3);
+
+console.log(Object.keys(obj1));             //give only keys.
+console.log(Object.values(obj1));           //give only value.
+console.log(Object.entries(obj1));          //every key value pair convert into array.
+console.log(obj1.hasOwnProperty(2));        //check value is in object or not.(return boolean answer.)
+
+
+
+
+
+
+
+
+
+
