969e8ca33c5377d1d6f35935d0b619f286ebe849
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-28 03:14:58 +0000

    objects-1

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,31 @@
+//---------------objects-----------------
+//singleton if only create using constructor.
+
+const mysyl = Symbol("key1");
+//object literals.
+const jsobj = {name : "Daxil", age : 20, location : "Surat",[mysyl] : "Lion"} //key use as a object.
+// console.log(jsobj.location);
+// console.log(jsobj["location"]);
+// console.log(jsobj["age"]);
+// console.log(jsobj[mysyl]);
+
+jsobj.age = 34              //age value change.
+// Object.freeze(jsobj)        //after freeze method any objects value can not be change. 
+jsobj.age = 23              //this age value don't change because of freeze method.
+// console.log(jsobj);
+
+jsobj.greeting = function(){
+    console.log("Hello users...");   
+}
+
+jsobj.greetingTwo = function(){
+    console.log(`hello ${this.name}`);   //technique to take any object value. 
+    
+}
+console.log(jsobj.greeting);        //return only function reference.
+console.log(jsobj.greeting());      //return funtion value.
+console.log(jsobj.greetingTwo());
+
+
+
+
