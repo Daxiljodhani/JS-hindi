3033ac39a20f6cbd172acc9272b5b4a676516be3
Daxil jodhani <dakshiljodhani3040@gmail.com>
2026-01-29 17:26:03 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+//------------------if----------------------
+
+// if(2 === "2"){                      //=== check not only equal it also check type of both.
+//     // console.log("executed");
+// }
+//<,>,<=,>=,==,!=,===
+
+// const score = 200
+// if(score > 100){
+//     let power = "fly"
+//     console.log(power);
+// }
+// console.log(power);              //this line can not be print because that is out of scope.
+
+const balance = 1000
+
+// if(balance > 500) console.log("wow"),console.log("bheem");
+
+// if(balance > 500){
+//     console.log("greater 500");
+// }
+// else if(balance < 700){
+//     console.log("less than 700");
+// }
+// else{
+//     console.log("other..");
+// }
+
+
+
+
